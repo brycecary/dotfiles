491d5ec3ae2edb7c04a63786596704179ba33c26
topmastered <bcary.md@gmail.com>
2020-05-18 03:17:29 -0400

    added awesomewm configs

---
+++ .config/awesome/configuration/client/rules.lua
@@ -0,0 +1,143 @@
+local awful = require('awful')
+local gears = require('gears')
+local client_keys = require('configuration.client.keys')
+local client_buttons = require('configuration.client.buttons')
+
+-- Rules
+awful.rules.rules = {
+  -- All clients will match this rule.
+  {
+    rule = {},
+    properties = {
+      focus = awful.client.focus.filter,
+      raise = true,
+      keys = client_keys,
+      buttons = client_buttons,
+      screen = awful.screen.preferred,
+      placement = awful.placement.no_offscreen,
+      floating = false,
+      maximized = false,
+      above = false,
+      below = false,
+      ontop = false,
+      sticky = false,
+      maximized_horizontal = false,
+      maximized_vertical = false,
+      opacity = 0.95,
+      tag = "8"
+    }
+  },
+  {
+    rule = { instance = "terminator" },
+    properties = {
+      screen = 1,
+      tag = "5",
+    }
+  },
+  {
+    rule = { instance = "gimp" },
+    properties = {
+      screen = 1,
+      tag = "8",
+    }
+  },
+  {
+    rule = { instance = "vivaldi-stable" },
+    properties = {
+      screen = 1,
+      tag = "1",
+      opacity = 1
+    }
+  },
+  {
+    rule = { instance = "chromium" },
+    properties = {
+      screen = 1,
+      tag = "1",
+      opacity = 1
+    }
+  },
+  {
+    rule = { instance = "discord" },
+    properties = {
+      screen = 1,
+      tag = "3",
+    }
+  },
+  {
+    rule = { instance = "Steam" },
+    properties = {
+      screen = 1,
+      tag = "7",
+    }
+  },
+  {
+    rule = { instance = "nautilus" },
+    properties = {
+      screen = 1,
+      tag = "4",
+    }
+  },
+  {
+    rule = { instance = "file-roller" },
+    properties = {
+      screen = 1,
+      tag = "4",
+    }
+  },
+  {
+    rule = { instance = "code - insiders" },
+    properties = {
+      screen = 1,
+      tag = "2",
+    }
+  },
+  {
+    rule = { instance = "code-insiders" },
+    properties = {
+      screen = 1,
+      tag = "2",
+    }
+  },
+  {
+    rule = { instance = "vlc" },
+    properties = {
+      screen = 1,
+      tag = "6",
+    }
+  },
+  {
+    rule = { instance = "spotify" },
+    properties = {
+      screen = 1,
+      tag = "6",
+    }
+  },
+  {
+    rule = { instance = "ts3client_linux_amd64" },
+    properties = {
+      screen = 1,
+      tag = "3",
+    }
+  },
+  {
+    rule_any = {name = {'QuakeTerminal'}},
+    properties = {skip_decoration = true}
+  },
+  -- Titlebars
+  {
+    rule_any = {type = {'dialog'}, class = {'Wicd-client.py', 'calendar.google.com'}},
+    properties = {
+      placement = awful.placement.centered,
+      ontop = true,
+      floating = true,
+      drawBackdrop = true,
+      shape = function()
+        return function(cr, w, h)
+          gears.shape.rounded_rect(cr, w, h, 8)
+        end
+      end,
+      skip_decoration = true
+    }
+  }
+}
---
+++ .config/awesome/theme/PapyElGringo-theme/init.lua
@@ -0,0 +1,27 @@
+local filesystem = require('gears.filesystem')
+local mat_colors = require('theme.mat-colors')
+local theme_dir = filesystem.get_configuration_dir() .. '/theme'
+local dpi = require('beautiful').xresources.apply_dpi
+
+local theme = {}
+theme.icons = theme_dir .. '/icons/'
+theme.font = 'Roboto medium 10'
+
+-- Colors Pallets
+
+-- Primary
+theme.primary = mat_colors.red
+
+-- Accent
+theme.accent = mat_colors.orange
+
+-- Background
+theme.background = mat_colors.grey
+
+local awesome_overrides = function(theme)
+  --
+end
+return {
+  theme = theme,
+  awesome_overrides = awesome_overrides
+}
